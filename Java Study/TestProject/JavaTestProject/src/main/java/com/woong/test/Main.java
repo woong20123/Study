972c2ca9102f6445b8f647371ad7362d04ca0ad7
woong20123 <62825883+woong20123@users.noreply.github.com>
2021-12-05 22:39:49 +0900

    2021년 12월 5일

--- Java Study/TestProject/JavaTestProject/src/main/java/com/woong/test/Main.java
+++ Java Study/TestProject/JavaTestProject/src/main/java/com/woong/test/Main.java
@@ -1,20 +1,19 @@
 package com.woong.test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+class Juicer {
+	static List<Number> makeJuice(List<? extends Number> datas){
+		return new ArrayList<Number>(datas);
+	}
+}
 
 public class Main {
 	public static void main(String args[]) {
 		List<Integer> list = new ArrayList<>();
-		String[] data = { "bat", "baby", "bonus", "cA", "ca", "co", "c.", "c0", "car", "combat", "count", "date",
-				"disc" };
-
-		Pattern p = Pattern.compile("c[a-z]*");
-
-		for (int i = 0; i < data.length; i++) {
-			Matcher m = p.matcher(data[i]);
-			if (m.matches())
-				System.out.print(data[i] + ",");
-		}
+		Juicer.makeJuice(list);
 	}
 }
